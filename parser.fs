58a9889dbd334bd5cba29a5ce7884ad7bf94968f
Stephen Olsen <steve.olsen@gmail.com>
2013-04-22 23:07:43 -0400

    repl

--- parser.fs
+++ parser.fs
@@ -255,11 +255,28 @@ let parseAndShow str =
     | Success(result, _, _)   -> printfn "Success: %s" (showVal result)
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
-let repl str =
+let evalAndPrint str =
   match run parseExpr str with
     | Failure(errorMsg,_,_) -> printfn "%s" (showErr (ParseError errorMsg))
     | Success(result,_,_)   ->
       match (eval result) with
-        | LispVal v   -> printfn "%s" (showVal v)
+        | LispVal v   -> printfn "=> %s" (showVal v)
         | LispError e -> printfn "%s" (showErr e)
 
+let rec repl () =
+  do printf "Scheve <$^&> "
+  let input = Console.ReadLine()
+  match input with
+    | "quit" -> 0
+    | null -> 0
+    | input ->
+      do evalAndPrint input
+      repl ()
+
+[<EntryPoint>]
+let main args =
+  match args with
+    | [|filename|] ->
+      do printf "Running a scheme file not yet implemented"
+      0
+    | _ -> repl ()
