222aec5de5c5a5b3a598f44643576b62783cc70b
Stephen Olsen <steve.olsen@gmail.com>
2013-04-22 23:08:03 -0400

    more repl

--- parser.fs
+++ parser.fs
@@ -138,7 +138,8 @@ let showErr error =
     | ParseError error -> "ParseError\n" + error
     | NotFunction f -> "Value is not a function: " + f
     | NumArgs (needed, got) -> "Wrong number of arguments\n" +
-                               "Needed: " + needed.ToString() + " Got: " + got.ToString()
+                               "Needed: " + needed.ToString() +
+                               "Got: " + got.ToString()
     | NotImplemented f -> "Functionality is not implemented: " + f
 
 // Primitives
@@ -146,6 +147,11 @@ let unpackInteger i =
   match i with
     | Integer i -> i
     | _         -> raise UncheckedType
+
+let unpackBool b =
+  match b with
+    | Bool b -> b
+    | _      -> raise UncheckedType
 
 let isInteger i =
   match i with
@@ -190,13 +196,61 @@ let boolBoolBinOp fn args =
     | [_; otherB]          -> LispError (TypeMismatch ("Bool", otherB))
     | x                    -> NumArgs (2, List.length x) |> LispError
 
+let car args =
+  match args with
+    | [List (head :: tail)]               -> head |> LispVal
+    | [DottedList ((head :: tail), last)] -> head |> LispVal
+    | [item] -> LispError (TypeMismatch ("List", item))
+    | x      -> NumArgs (1, List.length x) |> LispError
+
+let cdr args =
+  match args with
+    | [List (head :: tail)] -> List tail |> LispVal
+    // todo nil?
+    | [DottedList ([_], last)] -> last |> LispVal
+    | [DottedList ((head :: tail), last)] -> DottedList (tail, last) |> LispVal
+    | [item] -> LispError (TypeMismatch ("List", item))
+    | x      -> NumArgs (1, List.length x) |> LispError
+
+let cons args =
+  match args with
+    | [x; List []] -> List [x] |> LispVal
+    | [x; List xs] -> List (x :: xs) |> LispVal
+    | [x; DottedList (xs, last)] -> DottedList ((x :: xs), last) |> LispVal
+    | [x1; x2] -> DottedList ([x1], x2) |> LispVal
+    | x -> NumArgs (2, List.length x) |> LispError
+
+let rec eqv args =  
+  match args with
+    | [Bool a; Bool b] -> Bool (a = b) |> LispVal
+    | [Integer a; Integer b] -> Bool (a = b) |> LispVal
+    | [String a; String b] -> Bool (a = b) |> LispVal
+    | [Atom a; Atom b] -> Bool (a = b) |> LispVal
+    | [DottedList (xs, x); DottedList (ys, y)] ->
+      eqv [List (List.append xs [x]); List (List.append ys [y])]
+    | [List x; List y] ->
+      if (List.length x) = (List.length y) then
+        let eqvs = List.map2 (fun a b -> eqv [a; b]) x y in
+          match List.tryFind isLispError eqvs with
+            | Some e -> e
+            | None   ->
+              // if not error, eqv only returns bools
+              List.map getLispVal eqvs
+              |> List.map unpackBool
+              |> List.fold (&&) true
+              |> Bool |> LispVal
+      else
+        Bool false |> LispVal
+    | [_; _] -> Bool false |> LispVal
+    | x -> NumArgs (2, List.length x) |> LispError
+
 let primitives = Map [("+", numericOperator (+));
                       ("-", numericOperator (-));
                       ("*", numericOperator (*));
                       ("/", numericOperator (/));
                       ("mod", numericOperator (%));
                       //("quotient", numericOperator quot);
-                      //("remainder", numericOperator rem);                       
+                      //("remainder", numericOperator rem);
                       ("=", numBoolBinOp (=));
                       ("<", numBoolBinOp (<));
                       (">", numBoolBinOp (>));
@@ -213,6 +267,8 @@ let primitives = Map [("+", numericOperator (+));
                       ("car", car);
                       ("cdr", cdr);
                       ("cons", cons);
+                      ("eq?", eqv);
+                      ("eqv?", eqv);
                       ]
 
 // Evaluation
